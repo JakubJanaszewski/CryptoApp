14f81108c6732837cf5313c86bb3ea477f7d2c5b
JakubJanaszewski <jakubjanaszewskii@gmail.com>
2022-01-17 15:58:36 +0100

    CryptoApp_v1

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,15 +1,14 @@
 from typing import Optional
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
+from data_requests import *
 
 class Cryptocurrency:
-    def __init__(self, name: Optional[str] = None, symbol: Optional[str] = None,
+    def __init__(self, name: Optional[str] = None, symbol: Optional[str] = None, currency: Optional[str] = None,
                   price: Optional[float] = None, volume_24h: Optional[float] = None,
                   percent_change_1h: Optional[float] = None, percent_change_24h: Optional[float] = None,
-                 percent_change_7d: Optional[float] = None, percent_change_30d: Optional[float] = None):
+                  percent_change_7d: Optional[float] = None, percent_change_30d: Optional[float] = None):
          self.name = name
-         self.symbol= symbol
+         self.symbol = symbol
+         self.currency = currency
          self.price = price
          self.volume_24h = volume_24h
          self.percent_change_1h = percent_change_1h
@@ -17,43 +16,31 @@ class Cryptocurrency:
          self.percent_change_7d = percent_change_7d
          self.percent_change_30d = percent_change_30d
 
-    def __str__(self):
-         return f"[{self.name}] {self.symbol} {self.price}"
+    def __str__(self) -> str:
+         return f"[{self.name}] {self.symbol} {self.currency} {self.price}"
 
-def request():
-     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-     parameters = {
-          'start':'1',
-          'limit':'200',
-          'convert':'USD'
-     }
-     headers = {
-          'Accepts': 'application/json',
-          'X-CMC_PRO_API_KEY': 'c1fbd94f-1e45-4652-9524-541113224366',
-     }
-     session = Session()
-     session.headers.update(headers)
-     try:
-          response = session.get(url, params=parameters)
-          data = json.loads(response.text)
-          return data
-     except (ConnectionError, Timeout, TooManyRedirects) as e:
-          return e
-
-def build_cryptocurrency():
-     respose = request()
+def build_cryptocurrencies() -> list[Cryptocurrency]:
+     currencies = ["USD", "EUR", "PLN"]
      crypto_list = list()
-     for crypto in respose:
-          crypto_list.append(Cryptocurrency(crypto["name"], crypto["symbol"],
-                                            float(crypto["price"]), float(crypto["volume_24h"]),
-                                            float(crypto["percent_change_1h"]), float(crypto["percent_change_24h"]),
-                                            float(crypto["percent_change_7d"]), float(crypto["percent_change_30d"])))
+     for currency in currencies:
+         url, parameters = request_data(currency)
+         response = request(url, parameters)
+         for crypto in response["data"]:
+             crypto_list.append(Cryptocurrency(crypto["name"], crypto["symbol"], currency,
+                                                float(crypto["quote"][currency]["price"]), float(crypto["quote"][currency]["volume_24h"]),
+                                                float(crypto["quote"][currency]["percent_change_1h"]), float(crypto["quote"][currency]["percent_change_24h"]),
+                                                float(crypto["quote"][currency]["percent_change_7d"]), float(crypto["quote"][currency]["percent_change_30d"])))
      return crypto_list
 
-print(build_cryptocurrency())
+def get_cryptocurrency_name_list(cryptocurrencies: list = None) -> list[str]:
+    crypto_names = list()
+    for cryptocurrency in cryptocurrencies:
+        if cryptocurrency.name not in crypto_names:
+            crypto_names.append(cryptocurrency.name)
+    return crypto_names
 
-
-
-
-
-
+def get_cryptocurrency(cryptocurrencies: list = None, cryptocurrency: str = None, currency: str = "PLN") -> Cryptocurrency:
+    for crypto in cryptocurrencies:
+        if crypto.name == cryptocurrency and crypto.currency == currency:
+            return crypto
+    return Cryptocurrency()
